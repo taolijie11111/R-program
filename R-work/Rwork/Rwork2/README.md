77a91340f6de42bc2a65b8a70e8aa351dca94858
taolijie11111 <taolijie11111@qq.com>
2022-01-30 00:45:15 +0800

    add

---
+++ R-work/Rwork/Rwork2/README.md
@@ -0,0 +1,4 @@
+* update:
+1. rewrite Rwork.R (use function)
+2. output it into .txt
+3. video recoding
---
+++ R-work/Rwork/Rwork2/Rwork.R
@@ -0,0 +1,83 @@
+# Load packages
+library(shiny)
+library(dplyr)
+library(networkD3)
+
+
+# Load data
+MisLinks<-read.csv("/home/taolijie11111/文档/repositysGithub/R-program/R-work/Rwork/MisLinks.csv",header = TRUE)
+MisNodes<-read.csv("/home/taolijie11111/文档/repositysGithub/R-program/R-work/Rwork/MisNodes.csv",header = TRUE)
+MisNodes$link_id <- 1:nrow(MisNodes) - 1
+
+SearchNumber<-function(MisLinks,MisNodes){
+     cat("Start","\n","Please select the choice:","1:input number","2:view all numbers ",sep = "\n")
+     Chioces<-readline(prompt = "your choice: ")
+     if(Chioces==1){
+          cat("if you don't know the number,you can input 'help' to get numbers")
+          number<-readline(prompt = "please input your number: ")
+          if(number=="help"){
+               cat("numbers:",MisNodes$name)
+               number<-readline(prompt = "please input your number: ")
+          }
+          subnodes<-MisNodes[MisNodes$name ==number, ]
+          if(length(subnodes$name)==0){
+               return(print("i don't have this number，please run thsi file and try again. ")   )
+          }
+          r<-subnodes$link_id
+          a<-which(MisLinks$scoure==r)
+          b<-which(MisLinks$target==r)
+          if (length(a)==0) {
+               c<-b
+          }else if(length(b)==0){
+               c<-a
+          }else {
+               c<-cbind(a,b)
+          }
+          sublinks<-MisLinks[c,]
+          x<-MisNodes[sublinks$scoure+1,]
+          y<-MisNodes[sublinks$target+1,]
+          z<-rbind(x,y)
+          z<-unique(z)
+          subnodes<-z
+          subnodes$Number<-c(0:(length(z$name)-1))
+          z<-subnodes
+          names(z)[names(z) == "link_id"] <- "scoure"
+          D1<-inner_join(sublinks,z,by="scoure")
+          sublinks$scoure <- D1$Number
+          names(z)[names(z) == "scoure"] <- "target"
+          D1<-inner_join(sublinks,z,by="target")
+          sublinks$target <- D1$Number
+          
+          Network<-forceNetwork(sublinks, subnodes,"scoure", "target","value", "name", "size","group",
+                       height = 960, width = 960,
+                       colourScale = JS("d3.scaleOrdinal(d3.schemeCategory20);"), fontSize = 15,
+                       fontFamily = "黑体", linkDistance = 300,
+                       linkWidth = JS("function(d) { return Math.sqrt(d.value)*0.8; }"),
+                       radiusCalculation = JS("Math.sqrt(d.nodesize)+6"), charge = -30,
+                       linkColour = "#666", opacity = 1, zoom = FALSE, legend = FALSE,
+                       arrows =TRUE, bounded = FALSE, opacityNoHover = 0.5,
+                       clickAction = NULL)
+          print(Network)
+          cat("Task completed")
+          
+     }else if(Chioces==2){
+          Network<-forceNetwork(MisLinks, MisNodes,"scoure", "target","value", "name", "size","group",
+                       height = 960, width = 960,
+                       colourScale = JS("d3.scaleOrdinal(d3.schemeCategory20);"), fontSize = 15,
+                       fontFamily = "黑体", linkDistance = 300,
+                       linkWidth = JS("function(d) { return Math.sqrt(d.value)*0.8; }"),
+                       radiusCalculation = JS("Math.sqrt(d.nodesize)+6"), charge = -30,
+                       linkColour = "#666", opacity = 1, zoom = FALSE, legend = FALSE,
+                       arrows =TRUE, bounded = FALSE, opacityNoHover = 0.5,
+                       clickAction = NULL)
+          print(Network)
+          cat("Task completed")
+     }else{
+          return (print("i don't have this choice，please run this file and input again. "))
+     }
+     
+}
+
+SearchNumber(MisLinks,MisNodes)# use netMork
+
+
